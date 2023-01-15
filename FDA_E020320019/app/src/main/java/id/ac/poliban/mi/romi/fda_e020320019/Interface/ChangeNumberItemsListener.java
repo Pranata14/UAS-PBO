c85a34dd76c1de8f5c441dd409769bc31c14c248
Pranata14 <romipranata004@gmail.com>
2023-01-15 14:59:19 +0700

    UAS PBO

---
+++ FDA_E020320019/app/src/main/java/id/ac/poliban/mi/romi/fda_e020320019/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.mi.romi.fda_e020320019.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
---
+++ Project82_E020320019/app/src/main/java/id/ac/poliban/mi/romi/project82_e020320019/SignUpActivity.java
@@ -0,0 +1,14 @@
+package id.ac.poliban.mi.romi.project82_e020320019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
